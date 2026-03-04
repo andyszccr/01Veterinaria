d4ce31e037355900097ee351a5768f4fd27a6dde
andreyszccr <andreyszcr@gmail.com>
2026-03-04 14:48:13 -0600

    tabla de products and categories

---
+++ BDVeterinaria/dbo/Tables/Categories.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Categories] (
+    [CategoryCode]   CHAR (7)     NOT NULL,
+    [CategoryID]     INT          IDENTITY (1, 1) NOT NULL,
+    [CategoryName]   VARCHAR (50) NOT NULL,
+    [CategoryCreate] DATETIME     NULL,
+    [CategoryUpdate] DATETIME     NULL,
+    [CategoryDelete] DATETIME     NULL,
+    [CategoryStatus] BIT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([CategoryID] ASC)
+);
+
---
+++ BDVeterinaria/dbo/Tables/Products.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [ProductCode]   CHAR (7)        NOT NULL,
+    [ProductID]     INT             IDENTITY (1, 1) NOT NULL,
+    [ProductName]   VARCHAR (100)   NOT NULL,
+    [Description]   VARCHAR (200)   NULL,
+    [Price]         DECIMAL (10, 2) NOT NULL,
+    [ProductCreate] DATETIME        NULL,
+    [ProductUpdate] DATETIME        NULL,
+    [ProductDelete] DATETIME        NULL,
+    [ProductStatus] BIT             NOT NULL,
+    [CategoryID]    INT             NULL,
+    PRIMARY KEY CLUSTERED ([ProductID] ASC),
+    CONSTRAINT [FK_Products_Categories] FOREIGN KEY ([CategoryID]) REFERENCES [dbo].[Categories] ([CategoryID])
+);
+
